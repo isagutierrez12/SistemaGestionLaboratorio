4b132dfa725874043d1db87ba30aac56707dbeb6
Melany <123525805+melanyscode@users.noreply.github.com>
2025-09-15 23:59:13 -0600

    primeras configuraciones

---
+++ Laboratorio/src/main/java/com/laboratorio/repository/UsuarioRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.laboratorio.repository;
+import com.laboratorio.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+/**
+ *
+ * @author melanie
+ */
+@EnableJpaRepositories
+public interface UsuarioRepository extends JpaRepository < Usuario, Long> {
+
+}
