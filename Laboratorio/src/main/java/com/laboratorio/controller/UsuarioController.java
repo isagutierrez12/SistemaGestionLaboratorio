c0e3665f20277c08f5de1fe1ee35cd651aff7313
Melany <123525805+melanyscode@users.noreply.github.com>
2025-09-26 18:17:57 -0600

    listado de usuario

--- Laboratorio/src/main/java/com/laboratorio/controller/UsuarioController.java
+++ Laboratorio/src/main/java/com/laboratorio/controller/UsuarioController.java
@@ -3,6 +3,8 @@ package com.laboratorio.controller;
 
 
 
+import com.laboratorio.repository.UsuarioRepository;
+import com.laboratorio.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,11 +17,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @RequestMapping("/usuario")
 public class UsuarioController {
     
-   
+    @Autowired
+    UsuarioService usuarioService;
     
      @GetMapping("/usuarios")
     public String listadoUsuarios(Model model){
-        
+        var lista = usuarioService.getUsuarios();
+        model.addAttribute("usuarios", lista);
         return "/usuario/usuarios";
     }
 }
