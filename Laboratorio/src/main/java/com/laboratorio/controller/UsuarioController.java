2ea6098c09a698224bf028f6d466c4b0f4b5ed4c
Melany <123525805+melanyscode@users.noreply.github.com>
2025-09-29 16:32:18 -0600

    ajustes minimos

--- Laboratorio/src/main/java/com/laboratorio/controller/UsuarioController.java
+++ Laboratorio/src/main/java/com/laboratorio/controller/UsuarioController.java
@@ -1,6 +1,5 @@
 package com.laboratorio.controller;
 
-
 import com.laboratorio.model.Usuario;
 import com.laboratorio.repository.UsuarioRepository;
 
@@ -24,7 +23,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
@@ -32,22 +30,22 @@ public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
 
-    
- 
     @GetMapping("/desactivar/{id}")
     public String desactivar(@PathVariable Long id, RedirectAttributes redirectAttributes) {
         String mensaje = usuarioService.desactivarUsuario(id);
         redirectAttributes.addFlashAttribute("mensaje", mensaje);
         return "redirect:/usuario/usuarios";
-}
+    }
+
     @GetMapping("/reactivar/{id}")
     public String reactivar(@PathVariable Long id, RedirectAttributes redirectAttributes) {
         String mensaje = usuarioService.reactivarUsuario(id);
         redirectAttributes.addFlashAttribute("mensaje", mensaje);
-        return "redirect:/usuario/inactivos"; // página con usuarios desactivados
+        return "redirect:/usuario/usuarios"; // página con usuarios desactivados
     }
+
     @GetMapping("/buscar")
-    public String buscarUsuariosPorNombre(@RequestParam(value = "nombre", required = false) String nombre,Model model) {
+    public String buscarUsuariosPorNombre(@RequestParam(value = "nombre", required = false) String nombre, Model model) {
         if (nombre == null || nombre.trim().isEmpty()) {
             model.addAttribute("advertencia", "Debe ingresar al menos un criterio de búsqueda");
             return "usuario/usuarios"; // recarga la misma vista
@@ -63,6 +61,7 @@ public class UsuarioController {
             model.addAttribute("error", "No se pudo realizar la búsqueda. Intente nuevamente.");
         }
         return "usuario/usuarios";
+    }
 
     @Autowired
     RolService rolService;
@@ -98,7 +97,7 @@ public class UsuarioController {
         usuario = usuarioService.getUsuario(usuario);
 
         String rol = usuario.getRoles().getFirst().getNombre();
-        String rolSeleccionado ;
+        String rolSeleccionado;
         switch (rol) {
             case "ADMIN":
                 rolSeleccionado = "1";
@@ -113,11 +112,9 @@ public class UsuarioController {
                 rolSeleccionado = "";
 
         }
-        
+
         model.addAttribute("rol", rolSeleccionado);
         model.addAttribute("usuario", usuario);
         return "usuario/modificar";
     }
 }
-
-
