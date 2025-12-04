a8345912eeba0eccea0f583d60ded78168c5912e
Melany <123525805+melanyscode@users.noreply.github.com>
2025-12-03 21:40:19 -0600

    test-1

--- Laboratorio/src/main/java/com/laboratorio/ProjectConfig.java
+++ Laboratorio/src/main/java/com/laboratorio/ProjectConfig.java
@@ -20,7 +20,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
@@ -98,4 +101,21 @@ public class ProjectConfig implements WebMvcConfigurer {
         source.registerCorsConfiguration("/api/**", configuration);
         return source;
     }
+    // for deployment 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("index");
+    }
+    
+    @Bean
+    public SpringResourceTemplateResolver templateResolver_0(){
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/temnplates");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    }
+    
 }
